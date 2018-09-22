972bfc84e8ff7d692391d0a09367680218cef522
sasakipages <sasaki.kenta@outlook.com>
2018-09-22 21:21:07 +0900

    Gulpの導入

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ".",
+            index: "index.html"
+        }
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("*.html", ['bs-reload']);
+    gulp.watch("./css/*.css", ['bs-reload']);
+});
